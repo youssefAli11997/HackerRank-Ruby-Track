d1c805517455fbbda3025a766bc2a05bb7bd53c9
Youssef Ali <youssefali6212@gmail.com>
2017-09-22 19:21:49 +0300

    Create Ruby Array - Initialization.rb

---
+++ Arrays - Hashes/Ruby Array - Initialization.rb
@@ -0,0 +1,4 @@
+# Initialize 3 variables here as explained in the problem statement
+array = Array.new # or: array = []
+array_1 = Array.new(1) # or: array_1 = [nil]
+array_2 = Array.new(2,10) # or: array_2 = [10,10]
